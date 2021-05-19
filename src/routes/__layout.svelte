bee19b149a0c1277bc1814ef06aac195ec004d8e
Aaron Luna <contact@aaronluna.dev>
2021-05-18 19:23:38 -0700

    add analytics script to __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import { dev } from '$app/env';
 	import '../global.css';
 	import Nav from '$lib/components/Nav.svelte';
+
 </script>
 
+<svelte:head>
+	{@html dev
+		? ''
+		: '<script async defer data-website-id="add553f7-4504-4257-ae69-94e01630302c" src="https://umami.aaronluna.dev/umami.js"></script>'}
+</svelte:head>
+
 <Nav />
-<div class="wrapper">
-	<main role="main">
-		<slot />
-	</main>
-</div>
+<main role="main">
+	<slot />
+</main>
