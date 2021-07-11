b5562fac2ed9e3623c5422d005c54a8682438d66
Aaron Luna <contact@aaronluna.dev>
2021-07-10 20:01:20 -0700

    minor css changes and update dependencies

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -1,7 +1,6 @@
-// import { dev } from '$app/env';
+import { dev } from '$app/env';
 
-// const API_URL_ROOT_DEV = 'http://localhost:8000/api';
+const API_URL_ROOT_DEV = 'http://localhost:8000/api';
 const API_URL_ROOT_PROD = 'https://vig-api.aaronluna.dev/api';
-// export const API_URL_ROOT = dev ? API_URL_ROOT_DEV : API_URL_ROOT_PROD;
-export const API_URL_ROOT = API_URL_ROOT_PROD;
+export const API_URL_ROOT = dev ? API_URL_ROOT_DEV : API_URL_ROOT_PROD;
 export const API_VERSION = 'v1';
