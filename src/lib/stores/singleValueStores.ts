af477d4101b84439fd97eb5c18b77e60c216aa21
Aaron Luna <contact@aaronluna.dev>
2021-07-16 16:44:29 -0700

    update singleValueStores to default to 2021 season

--- src/lib/stores/singleValueStores.ts
+++ src/lib/stores/singleValueStores.ts
@@ -6,5 +6,5 @@ export const gameContentShown: Writable<GameContent> = writable('box');
 export const teamStatType: Writable<TeamStatType> = writable('bat');
 export const playerSeason: Writable<number> = writable(0);
 export const gameDate: Writable<Date> = writable(new Date(2021, 3, 1));
-export const season: Writable<number> = writable(2019);
+export const season: Writable<number> = writable(2021);
 export const siteTheme: Writable<SiteTheme> = createWritableSession('vig-theme', 'notset');
