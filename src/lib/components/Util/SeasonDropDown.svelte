f976f7b301339fdf865a4c94e6f06d6604f872f1
Aaron Luna <contact@aaronluna.dev>
2021-10-07 15:50:22 -0700

    persist allSeasons store to localstorage, create mostRecentSeason derived store Fixes #66

--- src/lib/components/Util/SeasonDropDown.svelte
+++ src/lib/components/Util/SeasonDropDown.svelte
@@ -1,63 +1,41 @@
 <script lang="ts">
-	import { getAllValidSeasons } from '$lib/api/season';
-	import type { ApiResponse, MlbSeason } from '$lib/api/types';
+	import type { MlbSeason } from '$lib/api/types';
 	import Select from '$lib/components/Select/Select.svelte';
-	import { allSeasons } from '$lib/stores/allMlbSeasons';
-	import type { SelectMenuOption } from '$lib/types';
-	import { createEventDispatcher, onMount } from 'svelte';
+	import { allSeasons, mostRecentSeason } from '$lib/stores/allMlbSeasons';
+	import { createEventDispatcher } from 'svelte';
 
 	export let width = 'auto';
-	export let currentSeason: number = 0;
-	let seasons: MlbSeason[];
-	let options: SelectMenuOption[] = [];
-	let getAllSeasonsRequest: Promise<ApiResponse<MlbSeason[]>>;
-	let getAllSeasonsResult: ApiResponse<MlbSeason[]>;
-	let menuLabel = 'Select Season';
-	let menuId = 'season';
-	const placeHolderMenuOption = [{ text: '', value: 0, optionNumber: 1, active: false }];
+	export let selectedValue: number = $mostRecentSeason.year;
+	export let selectedSeason: MlbSeason = $mostRecentSeason;
+	let selectComponent: Select;
+	const options = $allSeasons.map((s, i) => ({
+		text: s.year.toString(),
+		value: s.year,
+		optionNumber: i + 1,
+		active: false
+	}));
 	const dispatch = createEventDispatcher();
+	let menuId = 'season-menu';
 
-	$: menuLabel = currentSeason > 0 ? `MLB ${currentSeason}` : 'Select Season';
+	$: menuLabel = selectedValue ? `MLB ${selectedValue}` : 'Select Season';
 
-	async function getAllMlbSeasons(): Promise<ApiResponse<MlbSeason[]>> {
-		getAllSeasonsResult = await getAllValidSeasons();
-		if (!getAllSeasonsResult.success) {
-			return getAllSeasonsResult;
+	function handleSelectionChanged(year: number) {
+		if (selectedValue !== year) {
+			selectedValue = year;
+			const matches = $allSeasons.filter((s) => s.year === selectedValue);
+			if (matches.length > 0) {
+				selectedSeason = matches[0];
+				dispatch('changed', selectedSeason.start_date);
+			}
 		}
-		seasons = getAllSeasonsResult.value;
-		allSeasons.changeMlbSeasons(seasons);
-		return getAllSeasonsResult;
-	}
-
-	onMount(() => (getAllSeasonsRequest = getAllMlbSeasons()));
-
-	$: if (getAllSeasonsResult?.success) {
-		options = seasons.map((s, i) => ({
-			text: s.year.toString(),
-			value: s.year,
-			optionNumber: i + 1,
-			active: currentSeason === s.year
-		}));
-	}
-
-	function handleChanged(selectedSeason: number) {
-		currentSeason = selectedSeason;
-		dispatch('changed', selectedSeason);
 	}
 </script>
 
-{#if getAllSeasonsRequest}
-	{#await getAllSeasonsRequest}
-		<Select {menuLabel} options={placeHolderMenuOption} />
-	{:then _result}
-		{#if getAllSeasonsResult.success}
-			<Select {menuLabel} {options} {menuId} {width} on:changed={(e) => handleChanged(e.detail)} />
-		{:else}
-			<div class="error">Error: {getAllSeasonsResult.message}</div>
-		{/if}
-	{:catch error}
-		<div class="error">Error: {error.message}</div>
-	{/await}
-{:else}
-	<Select {menuLabel} options={placeHolderMenuOption} />
-{/if}
+<Select
+	bind:this={selectComponent}
+	{menuLabel}
+	{options}
+	{menuId}
+	{width}
+	on:changed={(e) => handleSelectionChanged(e.detail)}
+/>
