1475b2a56f4059e1fc8c29dfede70a9b9d85adf4
Aaron Luna <contact@aaronluna.dev>
2021-10-09 14:28:27 -0700

    change to node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,12 +10,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: adapter(),
 		target: '#svelte',
 
 		vite: {
